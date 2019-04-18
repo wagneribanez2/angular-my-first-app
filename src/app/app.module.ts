1d11c2a58f59ee9781549cf30c21c2a4f0d24d20
wagneribanez2 <wagneribanez2@hotmail.com>
2019-04-18 07:16:19 -0300

    aprendendo data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success.alert.component';
 import { WarningAlertComponent } from './warning-alert/warning.alert.component';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { WarningAlertComponent } from './warning-alert/warning.alert.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
